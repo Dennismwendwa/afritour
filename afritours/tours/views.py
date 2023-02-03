d892726d0fd8402cfe6cc77e00259c7e7856c459
Dennismwendwa <dennismusemb2@.com>
2023-02-03 07:35:53 +0300

    added Post model,

--- afritours/tours/views.py
+++ afritours/tours/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.urls import path
 
+from . models import Post
+
 
 # Create your views here.
 def city (request):
 
-    return render(request, 'tours/index.html')
+    post = Post.objects.all()
+
+
+    return render(request, 'tours/index.html', {'post': post})
 
 
 def home (request):
