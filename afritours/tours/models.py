d94fb85642163c228aa900513e43872c27bf8077
Dennismwendwa <dennismusemb2@.com>
2023-02-04 22:00:12 +0300

    added media folder and added layout features in the home.html

--- afritours/tours/models.py
+++ afritours/tours/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django .contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Post (models.Model):
@@ -9,10 +10,12 @@ class Post (models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True, upload_to='tours/image')
+    country = models.CharField(max_length=250, null=True, blank=True)
     category = models.CharField(max_length=250)
 
     
 
     def __str__(self):
 
-        return self.title + ' | ' + self.author
+        return self.title + ' | ' + str(self.author)
--- afritours/tours/views.py
+++ afritours/tours/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
 
 
-from . models import Post
+from .models import Post
 
 
 # Create your views here.
 def city (request):
 
-    post = Post.objects.all()
+    #object = Post.objects.all()
+
+    
+    
 
 
-    return render(request, 'tours/index.html', {'post': post})
+    return render(request, 'tours/index.html', {})
 
 
 def home (request):
 
-    return render(request, 'tours/home.html')
+    main = Post.objects.get(pk=2)
+    main_sub = Post.objects.get(pk=4)
+    shimba = Post.objects.get(pk=10)
+    park = Post.objects.get(pk=11)
+    titles = Post.objects.all()
+
+    
+
+    return render(request, 'tours/home.html', {'main': main, 'main_sub': main_sub, 'titles': titles, 'shimba': shimba, 'park': park}) 
+
+
+
+
+
+
+    
