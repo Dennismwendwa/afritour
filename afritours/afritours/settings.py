2c07e3b67246ad0e3a3c99c72a19af29d63d30aa
Dennismwendwa <dennismusemb2@.com>
2023-05-16 06:40:21 +0300

    updated settings file and storage

--- afritours/afritours/settings.py
+++ afritours/afritours/settings.py
@@ -81,14 +81,26 @@ WSGI_APPLICATION = 'afritours.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    	'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("PGDATABASE"),
+	'USER': os.environ.get("PGUSER"),
+	'PASSWORD': os.environ.get("PGPASSWORD"),
+	'HOST': os.environ.get("PGHOST"),
+	'PORT': os.environ.get("PGPORT"),
+
+    	}
+	}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -131,6 +143,7 @@ STATICFILES_DIRS = [
 ] 
 
 STATIC_ROOT = os.path.join(BASE_DIR / 'assets/')
+
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
--- afritours/tours/views.py
+++ afritours/tours/views.py
@@ -3,32 +3,36 @@ from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def city (request):
-
-    #object = Post.objects.all()
-    return render(request, 'tours/index.html', {})
 
 
+def city(request):
 
-def home (request):
+    # object = Post.objects.all()
+    return render(request, 'tours/index_test.html', {})
+
+
+def home(request):
 
     main = Post.objects.get(pk=2)
     main_sub = Post.objects.get(pk=4)
-    shimba = Post.objects.get(pk=10)
-    park = Post.objects.get(pk=11)
-    titles = Post.objects.all()[0:8]
-    slides = Post.objects.all()[7:13]
+    shimba = Post.objects.get(pk=4)
+    park = Post.objects.get(pk=5)
+    titles = Post.objects.all()[0:5]
+    slides = Post.objects.all()[2:5]
 
-    return render(request, 'tours/home.html', {'main': main, 'main_sub': main_sub, 'titles': titles, 
-                   'shimba': shimba, 
-                   'park': park,
-                   'slides':slides,
-                }) 
+    return render(request, 'tours/home.html', {'main': main, 'main_sub': main_sub, 'titles': titles,
+                                               'shimba': shimba,
+                                               'park': park,
+                                               'slides': slides,
+                                               })
+
+
 class Allposts(ListView):
     model = Post
     template_name = 'tours/allposts.html'
     ordering = ['-post_date']
-    #ordering = ['-id']
+    # ordering = ['-id']
+
 
 class DetailView(DetailView):
     model = Post
@@ -43,4 +47,4 @@ class CatListview(ListView):
         content = {
             'cat': self.kwargs['category'],
             'posts': Post.objects.filter(category=self.kwargs)
-            }
+        }
