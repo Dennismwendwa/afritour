edf252500d79feda0c79b982163f09a1faff2ba6
Dennismwendwa <dennismusemb2@.com>
2023-05-19 01:03:24 +0300

    added statics

--- afritours/afritours/settings.py
+++ afritours/afritours/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import dj_database_url
 from os.path import join, dirname
 from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
@@ -81,16 +82,14 @@ WSGI_APPLICATION = 'afritours.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASE_URL ="postgresql://postgres:YD6Kkdm0QFHBZYBeXBaU@containers-us-west-117.railway.app:8059/railway"
 
+DATABASES = {
+	"default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+
+"""
 DATABASES = {
     	'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -102,6 +101,7 @@ DATABASES = {
 
     	}
 	}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
