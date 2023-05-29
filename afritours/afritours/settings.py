a7655d5912b12940fb5a70ab329a890f299bd4f6
Dennismwendwa <dennismusemb2@.com>
2023-05-29 18:46:37 +0300

    changed db

--- afritours/afritours/settings.py
+++ afritours/afritours/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = "afritours.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASE_URL = "postgresql://postgres:YD6Kkdm0QFHBZYBeXBaU@containers-us-west-117.railway.app:8059/railway"
-
+"""
 DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
-
 """
+
 DATABASES = {
     	'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -101,7 +101,7 @@ DATABASES = {
 
     	}
 	}
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
