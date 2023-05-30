2772ac50d437ecc1c6ec8fbb3ddd782dca1eea78
Dennismwendwa <dennismusemb2@.com>
2023-05-30 10:32:14 +0300

    changed db

--- afritours/static/assets/js/morejs.js
+++ afritours/static/assets/js/morejs.js
@@ -3,12 +3,15 @@
   const overlayElement = document.querySelector('.overlay');
   const enlargedImageElement = overlayElement.querySelector('img');
 
+     // Hide the overlay container initially
+  overlayElement.style.display = 'none';
+
   // Add event listeners to each image
   popupTriggerElements.forEach(function (triggerElement) {
     triggerElement.addEventListener('click', function () {
       const imageSource = this.getAttribute('src');
       enlargedImageElement.setAttribute('src', imageSource);
-      overlayElement.style.display = 'block';
+      overlayElement.style.display = 'flex';
     });
   });
 
