54d48251a6bb831422f7828bdaac2acf973f384f
Dennismwendwa <dennismusemb2@.com>
2023-05-31 12:05:08 +0300

    updared

--- afritours/tours/models.py
+++ afritours/tours/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django .contrib.auth.models import User
 from datetime import date
 from django.urls import reverse
+# import for image compression
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from PIL import Image
+import io
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -26,7 +31,30 @@ class Post (models.Model):
     def __str__(self):
 
         return self.title + ' | ' + str(self.author)
-    
+
+
+#compressing images
+def compress_image(image):
+
+	img = Image.open(image)
+
+	#applying compression
+	img.save(image.file, 'JPG', quality=90, optimize=True)
+
+	#size image uncative for now
+	#img.thumbnail((800, 600))
+
+@receiver(pre_save, sender=Image)
+def compress_uploaded_image(sender, instance, **kwargs):
+	if instance.pk: #check instance is being updated
+		original_instance = sender.objects.get(pk=instance.pk)
+		if original_instance.image != instance.image:
+			compress_image(instance.image)
+	else: #new instance
+		compress_image(instance.image)
+
+
+
 
 class Image (models.Model):
 	name_of_park = models.CharField(max_length=250, blank=True, null=True)
